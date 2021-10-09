e50789e0fe85f5f3f6b6c4fcc9723515b0d84182
programmerJ <programmerJ@outlook.com>
2021-10-08 21:16:56 -0400

    add GemChartViewForm RadioButton

---
+++ DungeonsAndDragonsMainWindow/RadioButtonView/ViewMonetaryChartForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonsAndDragonsMainWindow.RadioButtonView
+{
+    /// <summary>
+    /// Interaction logic for ViewMonetaryChartForm.xaml
+    /// </summary>
+    public partial class ViewMonetaryChartForm : Window
+    {
+        public ViewMonetaryChartForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DungeonsAndDragonsMainWindow/ViewMagicItemChartForm.xaml.cs
+++ DungeonsAndDragonsMainWindow/ViewMagicItemChartForm.xaml.cs
@@ -9,71 +9,91 @@ namespace DungeonsAndDragonsMainWindow
     /// </summary>
     public partial class ViewMagicItemChartForm : Window
     {
-        GetCharts GetCharts; //todo what is with the 2nd getcharts? Why is it gray? I can get rid of it? Same with monsterTypes below?
+        GetCharts getCharts;
         MonsterTypes monsterTypes;
 
         public ViewMagicItemChartForm()
         {
             InitializeComponent();
-            GetCharts = new GetCharts();
+            getCharts = new GetCharts();
         }
         #region Monsters_A_To_K_ButtonClick
         private void Monster_Type_Abtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.A;
+            ChangeMonsterTypeLabel();
         }
 
         private void Monster_Type_Bbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.B;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Cbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.C;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Dbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.D;
+            ChangeMonsterTypeLabel();
         }
 
         private void Monster_Type_Ebtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.E;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Fbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.F;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Gbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.G;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Hbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.H;
+            ChangeMonsterTypeLabel();
+
 
         }
 
         private void Monster_Type_Ibtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.I;
+            ChangeMonsterTypeLabel();
+
 
         }
 
         private void Monster_Type_Jbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.J;
+            ChangeMonsterTypeLabel();
+
 
         }
 
         private void Monster_Type_Kbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.K;
+            ChangeMonsterTypeLabel();
+
 
         }
         #endregion Monsters_A_To_K_ButtonClick
@@ -82,85 +102,123 @@ namespace DungeonsAndDragonsMainWindow
         private void Monster_Type_Lbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.L;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Mbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.M;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Nbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.N;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Obtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.O;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Pbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.P;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Qbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.Q;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Rbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.R;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Sbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.S;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Tbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.T;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Ubtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.U;
+            ChangeMonsterTypeLabel();
+
         }
 
         private void Monster_Type_Vbtn_Click(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.V;
+            ChangeMonsterTypeLabel();
+
         }
         #endregion Monsters_L_To_V_ButtonClick
 
         #region MonetaryButtonClick
         private void Copperbtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeMagicItemChartLabel(MonetaryTreasureTypes.Copper);
         }
 
         private void Silverbtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeMagicItemChartLabel(MonetaryTreasureTypes.Silver);
         }
 
         private void Electrumbtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeMagicItemChartLabel(MonetaryTreasureTypes.Electrum);
         }
 
         private void Goldbtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeMagicItemChartLabel(MonetaryTreasureTypes.Gold);
         }
 
         private void Platinumbtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ChangeMagicItemChartLabel(MonetaryTreasureTypes.Platinum);
         }
         #endregion MonetaryButtonClick
 
+        private void ChangeMonsterTypeLabel()
+        {
+            Monsterlbl.Content = monsterTypes.ToString();
+        }
+
+        private void ChangeMagicItemChartLabel(MonetaryTreasureTypes treasure)
+        {
+            var magicItemTreasure = getCharts.MagicItemValue[monsterTypes];
+            Dicelbl.Content = magicItemTreasure.Dice.ToString();
+            Percentlbl.Content = magicItemTreasure.Percent.ToString();
+            Any_Magic_Itemslbl.Content = magicItemTreasure.AnyMagicItems.ToString();
+            Except_Weaponslbl.Content = magicItemTreasure.ExceptWeapons.ToString();
+            Number_Of_Rollslbl.Content = magicItemTreasure.NumberOfRolls.ToString();
+            Max_Roll_Valuelbl.Content = magicItemTreasure.MaxRollValue.ToString();
+            Magic_Item_Types_Formatted.Content = magicItemTreasure.MagicItemTypesFormatted.ToString();
+        }
     }
 }
