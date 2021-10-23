4cbca994f916d3fac16b9b357f09fcfd0192c642
programmerJ <programmerJ@outlook.com>
2021-10-22 23:56:34 -0400

    add Min & Max fields,  _GetNumberOfGems method, GetGemNumbers method, IncreaseGem method, Reset method, and add Gem properties to the logicRepository.cs,  add PercentageRoll, NumberOfGems, gemLabels and all of their values that change along with monsterType you selected in ViewGemChartForm.xaml  add MonsterTypeSelected.Content to each MonsterTypeA-V, add if statement to Roll_Click in ViewGemChartForm.xaml.cs

--- DungeonsAndDragonsMainWindow/ViewGemChartForm.xaml.cs
+++ DungeonsAndDragonsMainWindow/ViewGemChartForm.xaml.cs
@@ -11,7 +11,7 @@ namespace DungeonsAndDragonsMainWindow
     public partial class ViewGemChartForm : Window
     {
         GetCharts ChartEngine;
-        MonsterTypes monsterTypes = MonsterTypes.B;
+        MonsterTypes monsterTypes = MonsterTypes.B;// Don't think this is needed anymore
 
         public ViewGemChartForm()
         {
@@ -67,50 +67,71 @@ namespace DungeonsAndDragonsMainWindow
         public void MonsterTypeA(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.A;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         public void MonsterTypeB(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.B;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeC(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.C;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeD(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.D;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         public void MonsterTypeE(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.E;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         public void MonsterTypeF(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.F;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeG(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.G;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeH(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.H;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeI(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.I;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeJ(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.J;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeK(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.K;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeL(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.L;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         #endregion monsterTypesA-L
 
@@ -118,42 +139,59 @@ namespace DungeonsAndDragonsMainWindow
         public void MonsterTypeM(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.M;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeN(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.N;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeO(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.O;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeP(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.P;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeQ(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.Q;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeR(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.R;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeS(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.S;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
+
         }
         public void MonsterTypeT(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.T;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         public void MonsterTypeU(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.U;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         public void MonsterTypeV(object sender, RoutedEventArgs e)
         {
             monsterTypes = MonsterTypes.V;
+            Monster_Type_Selected.Content = $"You selected monster type: {monsterTypes}";
         }
         #endregion monsterTypesM-V
 
@@ -172,7 +210,30 @@ namespace DungeonsAndDragonsMainWindow
         {
             var logicRepository = new DungeonsAndDragons.ChartEngine.Services.LogicRepository();
             bool answer = logicRepository.PercentageRoll(monsterTypes);
+            string numberOfGems = "0";
             User_Dice_Roll.Content = answer.ToString();
+            Percentage_Roll.Content = logicRepository.NumberRolled;
+            if (answer)// does this always = true?
+            {
+                numberOfGems = logicRepository.GetNumberOfGems(monsterTypes);
+                NumberOfGems.Content = numberOfGems;
+                logicRepository.GetGemNumbers(numberOfGems);
+                Quartz.Content = logicRepository.Quartz.ToString();
+                Turquoise.Content = logicRepository.Turquoise.ToString();
+                Citrine.Content = logicRepository.Citrine.ToString();
+                Onyx.Content = logicRepository.Onyx.ToString();
+                Amber.Content = logicRepository.Amber.ToString();
+                Garnet.Content = logicRepository.Garnet.ToString();
+                Pearl.Content = logicRepository.Pearl.ToString();// This was logicRepository.Garnet. Fixed to dot Pearl. I think this will fix the for loop
+                Topaz.Content = logicRepository.Topaz.ToString();
+                Opal.Content = logicRepository.Opal.ToString();
+                Ruby.Content = logicRepository.Ruby.ToString();
+            }
+            else
+            {
+                numberOfGems = "0";
+                NumberOfGems.Content = "0";
+            }
         }
     }
 }
